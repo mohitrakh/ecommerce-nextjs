6ba4621b31f20ff73609983f2e3d6b134ed61ccf
mohit <mohitrakh01@gmail.com>
2024-04-23 20:45:10 +0530

    implimented stripe payement

--- src/app/(customerfacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerfacing)/products/[id]/purchase/page.tsx
@@ -1,12 +1,46 @@
 import db from "@/db/db"
 import { notFound } from "next/navigation"
+import Stripe from 'stripe'
+import { CheckoutForm } from "./_component/CheckoutForm"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY  as string)
 
 export default async function PurchasePage ({params : {id}} : {params: {id: string}}) {
-    const product = await db.product.findUnique({ where: {id}})
-    if (product == null) {
-        notFound()
+    const product = await db.product.findUnique({ where: { id } })
+    if (product == null) return notFound()
+  
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: product.priceInCents,
+      shipping: {
+        name
+        : 'Jenny Rosen',
+            address
+        : {
+              line1
+        : '510 Townsend St',
+              postal_code
+        : '98140',
+              city
+        : 'San Francisco',
+              state
+        : 'CA',
+              country
+        : 'US',
+            },
+      },
+      currency: "USD",
+  description: 'Software development services',
+      metadata: { productId: product.id },
+    })
+  
+    if (paymentIntent.client_secret == null) {
+      throw Error("Stripe failed to create payment intent")
     }
+  
     return (
-        <h1>new branch</h1>
+      <CheckoutForm
+        product={product}
+        clientSecret={paymentIntent.client_secret}
+      />
     )
 }
---
+++ src/app/(customerfacing)/products/download/[downloadVerificationId]/route.ts
@@ -0,0 +1,26 @@
+import db from "@/db/db";
+import { NextRequest, NextResponse } from "next/server";
+import fs from 'fs/promises'
+export async function GET(req: NextRequest, { params: {downloadVerificationId} } : {
+    params: { downloadVerificationId: string }
+}) {
+    const data = await db.downLoadVerification.findUnique({where: {
+        id: downloadVerificationId, expiresAt: { gt: new Date() }
+    },
+    select: {product:  {select: { filePath: true, name: true }}}
+});
+
+if (data == null) {
+    return NextResponse.redirect(new URL("/products/download/expired", req.url))
+}
+
+const {size} = await fs.stat(data.product.filePath)
+const file = await fs.readFile(data.product.filePath);
+const extention = data.product.filePath.split(".").pop()
+
+return new NextResponse(file, { headers: {
+        "Content-Disposition": `atttachment; filename="${data.product.name}.${extention}"`,
+        "Content-Length" : size.toString()
+    }
+})
+}
